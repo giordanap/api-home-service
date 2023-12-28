b711e984077230c416c6c7b0e1e2aca506ae653f
Giordan Arredondo <40145046+giordanap@users.noreply.github.com>
2023-12-28 08:52:45 -0500

    feat[Back]: Encrypt password
    using bcryptjs module

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,24 +1,34 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
-export const signup = async ( req, res ) => {
-    const { email, password, username } = req.body;
+export const signup = async (req, res) => {
+  const { email, password, username } = req.body;
 
-    try {
-        const newUser = new User({
-            username,
-            email,
-            password,
-        });
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
 
-        const userSaved = await newUser.save();
-        res.json( userSaved );
-    } catch (error) {
-        console.log(error);
-    }
-    
-    res.send('recording');
+    const newUser = new User({
+      username,
+      email,
+      password: passwordHash,
+    });
+
+    const userSaved = await newUser.save();
+
+    res.json({
+      id: userSaved.id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.send("recording");
 };
 
-export const signin = ( req, res ) => res.send('signin');
+export const signin = (req, res) => res.send("signin");
 
-export const profile = ( req, res ) => res.send('profile');
+export const profile = (req, res) => res.send("profile");
